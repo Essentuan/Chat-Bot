5a9eef857899612e170d775e242d80ff9796541d
Essentuan <51426319+Essentuan@users.noreply.github.com>
2019-09-13 19:09:28 -0500

    Added bot

--- chat/Geoff.js
+++ chat/Geoff.js
@@ -2,6 +2,10 @@ let words = []
 let awords = []
 let iwords = []
 var objDiv = document.getElementById("messages");
+let generate = 0
+let fWord = ""
+let bMessage = ""
+let lastWord = ""
 function getKeyCode(word) {
     let keycode = ""
     for (let i = 0; i < word.length; i++) {
@@ -9,14 +13,19 @@ function getKeyCode(word) {
     }
     return(keycode)
 }
+function random(array) {
+    let rn = Math.floor(Math.random() * array.length)
+    return(array[rn])
+}
 function addMessage(message, sender) {
     $("#messages").append("<b>" + sender +"</b><br><p>" + message + "</p><hr>")
+    if (sender != "Geoff") {
     let word = ""
     let preword = ""
     for (let i = 0; i <= message.length; i++) {
         if (message.charAt(i) == " " || message.charAt(i) == "") {
             if (iwords[getKeyCode(word)] == undefined) {
-                words.push(word);  
+                words.push(word);
                 iwords[getKeyCode(word)] = words.length
             } else {
                 console.log("Duplicate found! " + word)
@@ -25,7 +34,7 @@ function addMessage(message, sender) {
                 if (awords[getKeyCode(preword)] == undefined) {
                     awords[getKeyCode(preword)] = [word]
                 } else {
-                    awords[getKeyCode(preword)].push(word)    
+                    awords[getKeyCode(preword)].push(word)
                 }
             }
             preword = word
@@ -34,4 +43,23 @@ function addMessage(message, sender) {
             word = word + (message.charAt(i)).toString()
         }
     }
+    botReply()
+}
+}
+function botReply() {
+    generate = true
+    fWord = random(words)
+    bMessage = fWord + " "
+    lastWord = awords[getKeyCode(fWord)]
+    let nWord = 0
+    while (generate) {
+        if (lastWord == undefined) {
+            generate = false
+        } else {
+            nWord = random(lastWord)
+            lastWord = awords[getKeyCode(nWord)]
+            bMessage = bMessage + nWord + " "
+        }
+    }
+    addMessage(bMessage, "Geoff")
 }
